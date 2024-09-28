3f6abefd24841bff2abe4b2d68f6933002d2f3b3
mbelk059 <mbelk818@gmail.com>
2024-09-28 11:51:46 -0400

    navbar

--- insulin-coach/src/App.js
+++ insulin-coach/src/App.js
@@ -1,18 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+// src/App.js
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import 'rsuite/dist/rsuite.min.css'; // Import RSuite styles
 
 export default function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+    return (
+        <div style={{ display: 'flex' }}>
+            <BrowserRouter>
+                <Navbar />
+                <div style={{ marginLeft: '240px', padding: '20px' }}>
+                    <Routes>
+                        <Route index element={<Home />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
