ed15a6985a483eff7a4f0aaa56475eeb628345e8
mbelk059 <mbelk818@gmail.com>
2024-09-28 13:11:19 -0400

    push

--- insulin-coach/src/App.js
+++ insulin-coach/src/App.js
@@ -15,6 +15,7 @@ export default function App() {
                     <Routes>
                         <Route index element={<Home />} />
                         <Route path="/home" element={<Home />} />
+                        <Route path="/about" element={<About />} />
                         <Route path="/contact" element={<Contact />} />
                     </Routes>
                 </div>
