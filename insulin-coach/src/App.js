2b0e7bde6e6893827f7a22ba996b11dbe204670d
Saja Elkurtehi <114425874+Saja-elkurtehi@users.noreply.github.com>
2024-09-29 02:40:00 -0400

    adding stuff

--- insulin-coach/src/App.js
+++ insulin-coach/src/App.js
@@ -1,21 +1,24 @@
-// src/App.js
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Contact from './pages/Contact';
+import Contact from './pages/Contact'; 
 import Navbar from './components/Navbar';
-import 'rsuite/dist/rsuite.min.css'; // Import RSuite styles
+import 'rsuite/dist/rsuite.min.css'; 
+import Login from './components/Login/Login';
 
 export default function App() {
     return (
         <div style={{ display: 'flex' }}>
             <BrowserRouter>
                 <Navbar />
-                <div>
+                
+                <div style={{ flexGrow: 1, padding: '20px' }}>
                     <Routes>
                         <Route index element={<Home />} />
                         <Route path="/home" element={<Home />} />
                         <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<Contact />} /> 
+                        <Route path="/login" element={<Login />} />
                     </Routes>
                 </div>
             </BrowserRouter>
